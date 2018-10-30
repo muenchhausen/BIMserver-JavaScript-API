5c0ba135b443d3711a3d0333052b5a80ac395324
Derk Muenchhausen <derk.muenchhausen@siemens.com>
2018-10-30 21:29:25 +0100

    chore: working

--- test/integrationtest.ts
+++ test/integrationtest.ts
@@ -4,55 +4,111 @@ import {BimServerClient} from "../bimserverclient";
 describe('BimServerClient', () => {
 
     it('promised tests without chai-as-promised work in general', function () {
-        return Promise.resolve("bim")
+        let p = new Promise((resolve, reject) => {
+                resolve("bim");
+            })
+                .then((m) => {
+                    expect(m).to.equal('bim');
+                })
+                .catch((m) => {
+                    throw new Error('chai test failed');
+                })
+        ;
+        return p;
+    });
+
+    it('should return server information', function () {
+        return new Promise((resolve, reject) => {
+
+            let bimserverapi = new BimServerClient("http://localhost:8082");
+            //bimserverapi.login("admin@bimserver.org", "admin");
+
+            bimserverapi.call("AdminInterface", "getSystemInfo", {}, (data) => {
+                resolve(data);
+            }, (m) => {
+                throw new Error(JSON.stringify(m));
+            });
+
+        })
             .then((m) => {
-                expect(m).to.equal('bim');
+                expect(JSON.stringify(m)).to.contain('cpucores');
             })
             .catch((m) => {
-                throw new Error('chai test failed');
+                throw new Error(JSON.stringify(m));
             })
             ;
     });
-
 /*
-    it('should login with correct version', () => {
+    it('should login with correct version', (done) => {
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => reject(new Error("timeout")), 10000);
+
+            let bimserverapi = new BimServerClient("http://localhost:8082");
+
+            bimserverapi.login("derk@muenchhausen.de", "test", (data) => {
+                resolve(data);
+            }, (m) => {
+                throw new Error(JSON.stringify(m));
+            });
+
+        }).then((data) => {
+            expect(JSON.stringify(data)).to.contain("validationToken");
+        }).catch((m) => {
+            throw new Error('login failed ' + m);
+        });
+    });
+*/
+    /*
+    it('should return server information', () => {
 
 
-        return new Promise( (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             let bimserverapi = new BimServerClient("http://localhost:8082");
 
             bimserverapi.login("admin@bimserver.org", "xxxadmin", (data) => {
-               resolve(data);
+                resolve(data);
             }, (m) => {
-                throw new Error(m);
+                throw new Error(JSON.stringify(m));
             });
 
             setTimeout(() => reject(new Error("timeout")), 1000);
-        } ).then( (data) => {
+        }).then((data) => {
             expect(JSON.stringify(data)).to.contain("cpucores");
             // expect(bimserverapi.idCounter).to.equal(0);
-        }).catch( (m) => {
-            throw new Error('chai test failed');
+            return data;
+        }).then((data) => {
+            expect(JSON.stringify(data)).to.contain("cpucores");
+        }).catch((m) => {
+            throw new Error('login failed');
         });
-
-
-
     });
 */
-    it('should return server information', (done) => {
-        let bimserverapi = new BimServerClient("http://localhost:8082");
 
-        return bimserverapi.call("AdminInterface", "getSystemInfo", {},  (data) => {
-            console.log(JSON.stringify(data));
-            //    new Date(data.datetime)
-            //    data.osname
-            // data.osversion
-            //    data.userName
-            //   data.userHome  data.userDir
-            expect(JSON.stringify(data)).to.contain('cpucores');
-        },(m) => {
-            throw new Error(m);
-        });
-    });
+    /*
+            it('should return server information', (done) => {
 
+                let p = new Promise((resolve, reject) => {
+
+                     let bimserverapi = new BimServerClient("http://localhost:8082");
+                     bimserverapi.login("admin@bimserver.org", "admin");
+
+                     bimserverapi.call("AdminInterface", "getSystemInfo", {},  (data) => {
+                         //    new Date(data.datetime)
+                         //    data.osname
+                         // data.osversion
+                         //    data.userName
+                         //   data.userHome  data.userDir
+                         resolve(data);
+                     },(m) => {
+                         throw new Error(JSON.stringify(m));
+                     });
+                }).then((data) => {
+                    expect(JSON.stringify(data)).to.contain("cpucores");
+                }).catch((m) => {
+                    throw new Error('login failed');
+                });
+                return p;
+            });
+        */
 });
